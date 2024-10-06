31b4208427764badf826bc054f58bc9cf3c07cce
parowdude <parowdude@gmail.com>
2024-10-06 11:21:13 +0200

    Base for a hand/card renderer

---
+++ frontend/src/components/HandRenderer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hand">
+    <div class="card" v-for="(card, index) in playerHand" :key="index">
+      <img :src="getCardSvgUrl(card)" :alt="`${card.rank} of ${card.suit}`" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props:
+  {
+    playerHand:{required:true,type:Array}
+  }
+  ,methods: {
+    getCardSvgUrl(card) {
+      return `/assets/cards/${card.rank}_of_${card.suit}.svg`;
+    }
+  }
+};
+</script>
